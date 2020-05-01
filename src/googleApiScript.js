cc6146a939b78e9894f8b5c482848c8b895d16c3
GreenAvocado <greenavocado@protonmail.com>
2020-04-30 19:21:59 -0600

    feat: added authentication and writing

--- src/googleApiScript.js
+++ src/googleApiScript.js
@@ -1,8 +1,15 @@
+function authenticate() {
+return gapi.auth2.getAuthInstance()
+    .signIn({scope: "https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/spreadsheets"})
+    .then(function() { console.log("Sign-in successful"); },
+        function(err) { console.error("Error signing in", err); });
+}
+
 function loadClient() {
 gapi.client.setApiKey("AIzaSyDC6JNuMW78Q-gWsp0PFEaTsICYjHWymAo");
 return gapi.client.load("https://content.googleapis.com/discovery/v1/apis/sheets/v4/rest")
     .then(function() { console.log("GAPI client loaded for API"); },
-            function(err) { console.error("Error loading GAPI client for API", err); });
+        function(err) { console.error("Error loading GAPI client for API", err); });
 }
 
 function execute() {
@@ -12,11 +19,37 @@ function execute() {
     })
         .then(function(response) {
             console.log("Response", response);
-        },
+            },
+            function(err) { console.error("Execute error", err); });
+}
+
+function execute2() {
+    return gapi.client.sheets.spreadsheets.values.update({
+        "spreadsheetId": "1R0HpaAIUw-JHX8SrzvkEPCG1qgI-siJ9oucY6g5e4Co",
+        "range": "A1",
+        "includeValuesInResponse": true,
+        "responseValueRenderOption": "UNFORMATTED_VALUE",
+        "valueInputOption": "RAW",
+        "resource": {
+        "values": [
+            [
+            "Banana",
+            "Apricot"
+            ],
+            [
+            "acorn",
+            "pineapple"
+            ]
+        ]
+        }
+    })
+        .then(function(response) {
+            console.log("Response", response);
+            },
             function(err) { console.error("Execute error", err); });
 }
 
 gapi.load("client:auth2", function() {
-gapi.auth2.init();
+    gapi.auth2.init({client_id: "217251662395-9pu2qa1hubgrblav1nhvnfaascd6povv.apps.googleusercontent.com"});
 });
 
