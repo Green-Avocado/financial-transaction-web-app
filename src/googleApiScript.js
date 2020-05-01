ab7d2c8be4b37ba43d813bd1f873d591317aa4b3
GreenAvocado <greenavocado@protonmail.com>
2020-05-01 13:59:00 -0600

    feat: progress on read/write from google

--- src/googleApiScript.js
+++ src/googleApiScript.js
@@ -1,3 +1,39 @@
+var googleAuthenticated = false;
+
+function tableToArrays() {
+    var rows = document.getElementsByClassName('bodyRow');
+    var data = [];
+
+    for(var i = 0; i < rows.length; i++) {
+        var cells = rows[i].getElementsByTagName('td');
+        var cellData = [];
+
+        for(var j = 0; j < cells.length - 1; j++) {
+            cellData.push(cells[j].innerText);
+        }
+        data.push(cellData);
+    }
+
+    return data;
+}
+
+function arraysToTable(dataArr) {
+    while(document.getElementsByClassName('bodyRow').length > 0) {
+        document.getElementById("tableBody").removeChild(document.getElementsByClassName('bodyRow')[0]);
+    }
+
+    document.getElementById('add').removeAttribute('hidden');
+    document.getElementById('save').setAttribute('hidden', true);
+    document.getElementById('discard').setAttribute('hidden', true);
+
+    document.getElementById('add').setAttribute('type', 'submit');
+    document.getElementById('save').setAttribute('type', 'button');
+
+    for(var i = dataArr.length - 1; i >= 0; i++) {
+        addTransaction(dataArr[i]);
+    }
+}
+
 function authenticate() {
 return gapi.auth2.getAuthInstance()
     .signIn({scope: "https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/spreadsheets"})
@@ -12,44 +48,68 @@ return gapi.client.load("https://content.googleapis.com/discovery/v1/apis/sheets
         function(err) { console.error("Error loading GAPI client for API", err); });
 }
 
-function execute() {
+function readGoogleSheetDB() {
     return gapi.client.sheets.spreadsheets.values.get({
         "spreadsheetId": "1R0HpaAIUw-JHX8SrzvkEPCG1qgI-siJ9oucY6g5e4Co",
-        "range": "A1:C2"
+        "range": "A1:H214748354"
     })
         .then(function(response) {
             console.log("Response", response);
-            },
-            function(err) { console.error("Execute error", err); });
+            var dataArr = JSON.parse(response.Pf.body);
+            console.log(dataArr);
+        },
+        function(err) { console.error("Execute error", err); });
 }
 
-function execute2() {
-    return gapi.client.sheets.spreadsheets.values.update({
+function writeGoogleSheetDB() {
+    gapi.client.sheets.spreadsheets.batchUpdate({
         "spreadsheetId": "1R0HpaAIUw-JHX8SrzvkEPCG1qgI-siJ9oucY6g5e4Co",
-        "range": "A1",
-        "includeValuesInResponse": true,
-        "responseValueRenderOption": "UNFORMATTED_VALUE",
-        "valueInputOption": "RAW",
         "resource": {
-        "values": [
-            [
-            "Banana",
-            "Apricot"
-            ],
-            [
-            "acorn",
-            "pineapple"
-            ]
+        "requests": [
+            {
+            "updateSheetProperties": {
+                "properties": {
+                "gridProperties": {
+                    "rowCount": 1,
+                    "columnCount": 8
+                }
+                },
+                "fields": "gridProperties"
+            }
+            }
         ]
         }
     })
         .then(function(response) {
-            console.log("Response", response);
+            gapi.client.sheets.spreadsheets.values.clear({
+            "spreadsheetId": "1R0HpaAIUw-JHX8SrzvkEPCG1qgI-siJ9oucY6g5e4Co",
+            "range": "A1:H1",
+            "resource": {}
+        })
+            .then(function(response) {
+                console.log("Response", response);
+                return gapi.client.sheets.spreadsheets.values.update({
+                    "spreadsheetId": "1R0HpaAIUw-JHX8SrzvkEPCG1qgI-siJ9oucY6g5e4Co",
+                    "range": "A1",
+                    "includeValuesInResponse": true,
+                    "responseValueRenderOption": "UNFORMATTED_VALUE",
+                    "valueInputOption": "RAW",
+                    "resource": {
+                    "values": tableToArrays()
+                    }
+                })
+                    .then(function(response) {
+                        console.log("Response", response);
+                    },
+                    function(err) { console.error("Execute error", err); });
             },
             function(err) { console.error("Execute error", err); });
+        },
+        function(err) { console.error("Execute error", err); });
 }
 
 gapi.load("client:auth2", function() {
     gapi.auth2.init({client_id: "217251662395-9pu2qa1hubgrblav1nhvnfaascd6povv.apps.googleusercontent.com"});
+    loadClient();
 });
 
