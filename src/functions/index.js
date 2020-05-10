da73c0986f3e338fbe9cdb096b180d0657ca6a20
GreenAvocado <greenavocado@protonmail.com>
2020-05-09 21:14:26 -0600

    feat: connected to db, set up firebase api to work in localhost

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,8 +1,21 @@
 const functions = require('firebase-functions');
 const express = require('express');
+const mysql = require('mysql');
 const cors = require('cors');
 const api = express();
 api.use(cors({ origin: true }));
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "user",
+  password: "pass",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 api.get('/api', (req, res) => {
   return res.status(200).json({ hello: 'World' })
