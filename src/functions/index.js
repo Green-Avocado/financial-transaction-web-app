f5996d2f8a32e7bd011e1fefdd1f38dca4880b1a
GreenAvocado <greenavocado@protonmail.com>
2020-05-09 21:46:47 -0600

    feat: added post handling to node backend

--- src/functions/index.js
+++ src/functions/index.js
@@ -6,19 +6,31 @@ const api = express();
 api.use(cors({ origin: true }));
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "user",
-  password: "pass",
-  database: "mydb"
+    host: "localhost",
+    user: "user",
+    password: "pass",
+    database: "mydb"
 });
 
 con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+    if (err) throw err;
+    console.log("Connected to MySQL Database!");
+});
+
+function readFromMySQL() {
+}
+
+function writeToMySQL() {
+}
+
+api.post('/api', (req, res) => {
+    console.log(req.body);
+    return res.status(200).send(req.body);
 });
 
 api.get('/api', (req, res) => {
-  return res.status(200).json({ hello: 'World' })
+    console.log(req.body);
+    return res.status(200).json({ hello: 'World' });
 });
 
 exports.api = functions.https.onRequest(api);
