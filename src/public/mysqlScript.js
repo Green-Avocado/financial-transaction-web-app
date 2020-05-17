7569d93e6108b1b3175379e179eb6c87a26a0a12
GreenAvocado <greenavocado@protonmail.com>
2020-05-16 20:37:42 -0600

    fix: clear table before reading from MySQL

--- src/public/mysqlScript.js
+++ src/public/mysqlScript.js
@@ -19,6 +19,10 @@ function readFromMySQL() {
         .then(fullresponse => {
             console.log(fullresponse);
 
+            while(document.getElementsByClassName('bodyRow').length > 0) {
+                document.getElementById("tableBody").removeChild(document.getElementsByClassName('bodyRow')[0]);
+            }
+
             var data = fullresponse[0];
             for(var i = data.length - 1; i >= 0; i--) {
                 addTransaction([data[i].id, data[i].date, data[i].account, data[i].type, data[i].security, data[i].amount, data[i].dAmount, data[i].costBasis]);
